aed4d73cd81135f874880f6a726dbac8525d5d84
Rupesh Sahu <rupeshsahu969@gmail.com>
2025-09-24 20:11:34 +0530

    ready for netlify deployments

--- src/app/ClientProvider.tsx
+++ src/app/ClientProvider.tsx
@@ -4,7 +4,6 @@ import { ReactNode, useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-
 const queryClient = new QueryClient();
 
 export default function ClientProvider({ children }: { children: ReactNode }) {
