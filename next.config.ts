84bb539c136a410da3d5774c84cbdaccfac17f86
Rupesh Sahu <rupeshsahu969@gmail.com>
2025-09-24 17:41:12 +0530

    add this all data

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
-// next.config.ts
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,  
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'], 
-  trailingSlash: true,  
+  reactStrictMode: true,
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  trailingSlash: true,
+  webpack(config) {
+    // Map @ to the src folder
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
